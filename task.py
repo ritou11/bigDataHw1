ade37c89e3d58604532057e3170c2e35763135a9
ritou11 <ritou11@gmail.com>
2018-10-31 18:59:08 +0800

    variance homo

--- task.py
+++ task.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(color_codes=True)
 sns.set_style({
-    'font.family': '.PingFang SC',
+#    'font.family': '.PingFang SC',
+    'font.family': 'STSong',
     'axes.unicode_minus': False
 })
 import pandas as pd
@@ -35,7 +36,7 @@ plt.savefig('report/figure/task2-boxplot.png', dpi=300)
 
 # task 3
 def plot_pdf(choice):
-    plt.figure()
+    plt.clf()
     sns.distplot(projData[choice])
     plt.title('%sPDF图' % choice)
     plt.xlabel('%s/岁' % choice)
@@ -45,38 +46,51 @@ plt.savefig('report/figure/task3-pdf.png', dpi=300)
 
 # task 4
 def doThreeTest(group, choice):
-    ntN, ntP = stats.normaltest(group[choice])
+    ksD, ksP = stats.kstest((group[choice] - group[choice].mean()) / group[choice].std(), 'norm')
+    ntN, ntP = stats.normaltest((group[choice] - group[choice].mean()) / group[choice].std())
     spW, spP = stats.shapiro(group[choice])
     resStr = 'Skew and Kurtosis Test: N=%s, P=%s\n' % (ntN, ntP)
     return resStr, (ntN, ntP), (spW, spP)
 
-with open('report/task4q1.log', 'w') as resFile:
+with open('report/task4q1.log', 'w', encoding='utf8') as resFile:
     print(doThreeTest(projData, '平均年龄')[0], file=resFile)
 
-with open('report/task4q2.log', 'w') as resFile:
+with open('report/task4q2.log', 'w', encoding='utf8') as resFile:
     for name, group in projData.groupby('群类别'):
         print('Group %s' % name, file=resFile);
         print(doThreeTest(group, '平均年龄')[0], file=resFile)
 
-with open('report/task4q3.log', 'w') as resFile:
+with open('report/task4q3.log', 'w', encoding='utf8') as resFile:
     lm=ols('平均年龄 ~ C(群类别)', data=projData).fit()
     reportTable = sm.stats.anova_lm(lm, typ=1)
     print(reportTable, file=resFile)
 
 # task 5
 choices = ['性别比', '无回应比例', '夜聊比例']
-with open('report/task5norm.log', 'w') as resFile:
+with open('report/task5norm.log', 'w', encoding='utf8') as resFile:
     for c in choices:
         plot_pdf(c)
         plt.savefig('report/figure/task5-%s-pdf.png' % c, dpi=300)
         resStr = doThreeTest(projData, c)[0]
         print('特征：%s' % c, file=resFile)
         print(resStr, file=resFile)
-        plt.figure()
+        plt.clf()
         sns.distplot(projData[c], hist_kws={'log':True})
         plt.title('%s log PDF图' % c)
         plt.xlabel('%s/岁' % c)
         plt.savefig('report/figure/task4-%s-logpdf.png' % c, dpi=300)
         ntN, ntP = stats.normaltest(np.log(0.0000001 + projData[c]))
         print('Skew and Kurtosis Test: N=%s, P=%s\n' % (ntN, ntP))
-    
+with open('report/task5var.log', 'w', encoding='utf8') as resFile:
+    task5std = projData.groupby('群类别')['平均年龄'].std()
+    print(task5std, file=resFile)
+    print('Max std: %.3f, type %d, %s' % (task5std.max(), 
+                                               task5std.idxmax(), 
+                                               category.loc[task5std.idxmax()]['主题']),
+            file=resFile)
+    print('Min std: %.3f, type %d, %s' % (task5std.min(), 
+                                               task5std.idxmin(), 
+                                               category.loc[task5std.idxmin()]['主题']),
+            file=resFile)
+    print('MaxStd / MinStd = %.3lf' % (task5std.max() / task5std.min()),
+            file=resFile)
